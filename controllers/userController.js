6e95230ffff79d35b92b48f1eb2e54e64944d403
IchalAlwiros <alwiros99@gmail.com>
2022-12-13 14:06:30 +0700

    user controllers & router

---
+++ controllers/userController.js
@@ -0,0 +1,281 @@
+const { User } = require('../models')
+
+const Validator = require("fastest-validator");
+
+const v = new Validator();
+
+// CREATE USER (SIGNUP)
+const signup = async (req, res, next) => {
+    let {username, email, fullname, password, image, bio}= req.body
+    // res.render('index', {title: 'Sign Up User'})
+    const data = {
+        username,
+        email,
+        fullname,
+        password,
+        bio,
+        image,
+        createdAt : new Date(),
+        updatedAt : new Date(),
+        createdBy : 0,
+        updateBy : 0,
+        isDeleted : false
+    }
+
+
+    const schema = {
+        username : {type: 'string', min: 5, max:50, optional: false },
+        email : {type:"email", optional: false },
+        password : {type: 'string', min: 5, max:50, optional: false },
+    };
+
+  
+    // VALIDASI EMAIL
+    try {
+        const findUniqeEmail = await User.findOne({
+            where: {
+                email
+            }
+        })
+        
+        if (findUniqeEmail) {
+            // Email sudah digunakan
+            return res.status(400).json({
+                message : `Email ${findUniqeEmail.email} already exist`,
+            })
+          
+        }else{
+            // VALIDASI DATA
+            const validasiResult = v.validate(data, schema)
+    
+            if (validasiResult !== true) {
+                // Data tidak valid
+                return res.status(400).json({
+                    message : 'Validation Failed',
+                    data : validasiResult
+                })
+            } else {
+                // Create user jika belum di gunakan & data valid bisa di simpan
+                try {
+                    const userData = await User.create(data)
+                    return res.status(200).json({message : 'Success', data: userData})
+                } catch (error) {
+                    return res.status(400).json({
+                        message : 'Register Failed',
+                        error : error
+                    })
+                }
+            }        
+        }
+    } catch (error) {
+        return res.status(400).json({
+            message : 'Something wrong!',
+            error : error
+        })
+    }
+    
+}
+ 
+// READ USER
+const read = async (req, res, next) => {
+    // res.render('index', {title: 'Read User Data'})
+    try {
+        const userData = await User.findAll({
+            where : {
+                isDeleted : false
+            }
+        })
+        return res.status(200).json(userData)
+    } catch (error) {
+        return res.status(200).json({
+            error : error
+        })
+    }
+}
+
+// READ USER BY ID
+const readByid = async (req, res, next) => {
+    let {id} = req.params
+
+    // res.render('index', {title: `Read User Data ID ${id} `})
+    // try {
+    //     const userData = await User.findAll({
+    //         where : {
+    //             id
+    //         }
+    //     })
+    //     return res.status(200).json(userData)
+    // } catch (error) {
+    //     return res.status(200).json({
+    //         error : error
+    //     })
+    // }
+    try {
+        const userData = await User.findByPk(id)
+        return res.status(200).json(userData)
+    } catch (error) {
+        return res.status(200).json({
+            error : error
+        })
+    }
+}
+
+// UPDATE USER 
+const update = async (req, res, next) => {
+    let {id} = req.params
+    
+    // res.render('index', {title: `Update User Data ID ${id} `})
+    let {username, email, fullname, password, image, bio}= req.body
+    // res.render('index', {title: 'Sign Up User'})
+    const data = {
+        username,
+        email,
+        fullname,
+        password,
+        bio,
+        image,
+        updatedAt : new Date(),
+        updateBy : 0,
+        isDeleted : false
+    }
+
+    const schema = {
+        username : {type: 'string', min: 5, max:50, optional: false },
+        email : {type:"email", optional: false },
+        password : {type: 'string', min: 5, max:50, optional: false },
+    };
+
+    try {
+    // VALIDASI DATA
+    const validasiResult = v.validate(data, schema)
+    
+    if (validasiResult !== true) {
+        // Data tidak valid
+        return res.status(400).json({
+            message : 'Validation Failed',
+            data : validasiResult
+        })
+    } else {
+        // Update user jika data valid bisa di simpan
+       try {
+        const userData = await User.update(data, {where : {id}} )
+        return res.status(200).json({message : 'Success Update Data', data: userData})
+       } catch (error) {
+            return res.status(400).json({
+                message : 'Update Failed',
+                error : error
+            })
+        }
+    }   
+    } catch (error) {
+        return res.status(400).json({
+            message : 'Update Failed',
+            error : error
+        })
+    }
+
+    
+
+    // try {
+    //     const userData = await User.update(data, {where : {id}} )
+    //     return res.status(200).json({message : 'Success Update Data', data: userData})
+    // } catch (error) {
+    //     return res.status(400).json({
+    //         message : 'Update Failed',
+    //         error : error
+    //     })
+    // }
+
+}
+
+// DELETE USER
+const destroy = async (req, res, next) => {
+    let {id} = req.params
+    
+    // res.render('index', {title: `Delete User Data ID ${id} `})
+
+    // DELETE ROCORD
+    // try {
+    //     const userData = await User.destroy({where : {id}})
+    //     return res.status(200).json({
+    //         message : `Deleted data id ${id} success`,
+    //         data: userData
+    //     })
+    // } catch (error) {
+    //     return res.status(400).json({
+    //         message : 'Deleted Failed',
+    //         error : error
+    //     })
+    // }
+
+    // SOFT DELETE
+    const data = {
+        isDeleted : true,
+        deletedAt : new Date(),
+        deletedBy : 1,
+    }
+
+    try {
+        const userData = await User.update(data, {where : {id}} )
+        return res.status(200).json({message : 'Success Delete Data', data: userData})
+    } catch (error) {
+        return res.status(400).json({
+            message : 'Delete Failed',
+            error : error
+        })
+    }
+
+}
+
+// LOGIN USER (SIGNIN)
+const signin = async (req, res, next) => {
+    let {username, email, fullname, password, image, bio}= req.body
+
+    // res.render('index', {title: 'Sign In User'})
+    try {
+        const userData = await User.findOne({
+            where : {
+                email : email
+            }
+        })
+
+        console.log(userData.password);
+
+        if (userData) {
+            if (userData.isDeleted === false) {
+                if (userData.password === password) {
+                    return res.status(200).json({
+                        message: 'Success',
+                        data : userData
+                    })
+                } else {
+                    return res.status(400).json({
+                        message : 'Wrong Password',
+                        data : userData
+                    })
+                }
+            } else {
+                return res.status(401).json({
+                    message : 'User has been deleted',
+                    data : userData
+                })
+            }
+        } else {
+            return res.status(401).json({
+                message : 'Email not found',
+                data : userData
+            })
+        }
+    } catch (error) {
+        return res.status(500).json({
+                message : 'Login Failed',
+                data : error 
+            })
+    }
+}
+
+module.exports = {
+    signup, signin, 
+    readByid, read,
+    update, destroy,
+}
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+const userController = require('../controllers/userController')
+
+
+/* GET home page. */
+router.get('/', userController.read);
+router.get('/:id', userController.readByid);
+router.post('/signup', userController.signup);
+router.post('/signin', userController.signin);
+router.patch('/:id', userController.update);
+router.delete('/:id', userController.destroy);
+
 
 module.exports = router;
